dbf33c6bc16f20cbd11cf0df673caf9f84a8f77b
Jay Cheroske <jay@portal.org>
2017-03-08 11:11:26 -0800

    After fork

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+import 'babel-polyfill';
 import { greeting } from 'greeting';
 
 console.log(greeting());
+
+export const lambda = foo => console.log(foo);
+
+@lambda
+class Bar {
+  instanceVar;
+
+  constructor () {
+    console.log('in constructor');
+  }
+
+  whoop = () => console.log(this.instanceVar);
+}
+
+const bar = new Bar();
+
+console.log(bar);
